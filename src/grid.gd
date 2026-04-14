65e6f36ab963630842b213348213b7bc702c29b7
Jardel <jardelguim23@gmail.com>
2026-04-14 08:50:19 -0300

    grid and bricks fix

--- src/grid.gd
+++ src/grid.gd
@@ -1,24 +1,31 @@
 extends Node2D
 
-@export var columns : int = 9
+@export var columns : int = 20
 @export var rows : int = 3
 @export var brick : PackedScene
-var brick_height = 8
-var brick_width = 28
-var x_offset = 20
-var y_offset = 20
+var brick_height = 10
+var brick_width = 10
+var x_offset = 17
+var y_offset = 15
 var padding = 5
+
+@onready var brick_container: Marker2D = $BrickContainer
+
+func _ready() -> void:
+	pass
+	#start_grid()
 
 func start_grid() -> void:
 	'''Função para gerar um novo grid de bricks'''
 	for x in range(columns):
 		for y in range(rows):
-			var new_brick = brick.instantiate()
-			var x_pos = x_offset + x * (brick_width + padding)
-			var y_pos = y_offset + y * (brick_height + padding)
-			new_brick.position = Vector2(x_pos , y_pos)
-			add_child(new_brick)
-	
+			var randomNumber = randi_range(0 , 2)
+			if randomNumber > 0:
+				var new_brick = brick.instantiate()
+				var x_pos = x_offset + x * (brick_width + padding)
+				var y_pos = y_offset + y * (brick_height + padding)
+				new_brick.position = Vector2(x_pos , y_pos)
+				brick_container.add_child(new_brick)
 	
 func _add_brick(brick_height , brick_width) -> void:
 	pass
