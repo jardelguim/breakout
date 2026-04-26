9830d63dd422dc0e31b34a1c4de3d2756f252660
Flávio Vitoriano <flaviovitoriano.pessoal@gmail.com>
2026-04-26 20:29:08 -0300

    improve grid algoritm

--- src/grid.gd
+++ src/grid.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 const max_columns = 20
-@export var columns : int = 20
+const max_screen_size = 320
+@export var columns : int = 8
 @export var rows : int = 3
 @export var brick : PackedScene
 var brick_height = 15
 var brick_width = 15
-var x_offset = brick_width + 3
+var left_screen_offset = 0
+var x_offset = 18 + ((brick_width * (max_columns - columns)/2))
 var y_offset = 15
-var padding = 0
 
 
 @onready var brick_container: Marker2D = $BrickContainer
@@ -22,15 +23,17 @@ func _ready() -> void:
 func start_grid() -> void:
 	'''Função para gerar um novo grid de bricks'''
 	var bricks : Array
+
 	for line in range(rows):
 		var line_array = []
 		var line_brick_type = GameManager.brick_types.pick_random()
 		var color = GameManager.brick_colors.pick_random()
+		
 		for col in range(columns):
 			var new_brick = brick.instantiate()
 			# Set brick position
 			var pos = Vector2(
-				col * brick_width + x_offset, y_offset + line * brick_height
+				left_screen_offset + (col * brick_width + x_offset), y_offset + line * brick_height
 			)
 			# Set brick color and score given based on type
 			new_brick.brick_type = line_brick_type
@@ -41,3 +44,5 @@ func start_grid() -> void:
 			line_array.append(new_brick)
 			brick_container.add_child(new_brick)
 		bricks.append(line_array)
+	
+	var last_brick = bricks[-1][-1]
