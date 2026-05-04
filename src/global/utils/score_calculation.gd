386f49c3bdf23833695e72b723595261176891ff
Flávio Vitoriano <flaviovitoriano.pessoal@gmail.com>
2026-05-04 19:28:37 -0300

    improve calcuation feature and better events

--- src/global/utils/score_calculation.gd
+++ src/global/utils/score_calculation.gd
@@ -1,22 +1,28 @@
 extends Node
 
-signal update(score, multiplier)
+signal on_game_score_change(score)
+signal on_game_multiplier_change(multiplier)
 
-var multiplier: float = 1.0
+var multiplier: float = 1.0: set = _on_multiplier_set
 var multiplier_limit: float = 2.5
-var score: int = 0
+var score: int = 0: set = _on_score_set
+
+func _on_multiplier_set(value):
+	multiplier = value
+	on_game_multiplier_change.emit(value)
+
+func _on_score_set(value):
+	score = value
+	on_game_score_change.emit(int(value))
 
 func reset_chain():
 	multiplier = 1.0
-	update.emit(score, multiplier)
 
 func add_score_with_multiplication(base_score: int):
 	score += base_score * multiplier
-	update.emit(score, multiplier)
 
 func add_multiplier(multifactor: float = 0.1):
 	multiplier = clamp(multifactor + multiplier, 1.0, multiplier_limit)
-	update.emit(score, multiplier)
 
 func add_score_and_multiply(base_score, multifactor):
 	add_score_with_multiplication(base_score)
--- src/ui.gd
+++ src/ui.gd
@@ -13,24 +13,36 @@ func _process(delta: float) -> void:
 	label.text = str(ball.velocity)
 	
 func _ready() -> void:
-	ScoreCalculator.connect("update" , _update_text)
-	
+	ScoreCalculator.connect("on_game_score_change" , _on_game_score_update)
+	ScoreCalculator.connect("on_game_multiplier_change" , _on_game_multiplier_update)
+
 func _on_start_button_pressed() -> void:
 	$MainMenu.hide()
-	
-func _update_text(game_score , game_multiplier):
-	print("multiplicador:%s" %game_multiplier)
-	print("shake rate:%s" %shake_rate)
-	print("fire amount:%s" %multiplier_fire_particles.amount_ratio)
-	print("level:%s" %level )
+
+func _on_game_score_update(score):
+	_play_score_animation()
+	label_score.text = str(score)
+
+func _on_game_multiplier_update(multiplier):
 	_play_multi_animation()
-	if game_score != game_score:
-		_play_score_animation()
-	shake_rate = clamp(0 + ((game_multiplier - 1) * 33.3) , 0 , 50)
-	multiplier_fire_particles.amount_ratio = clamp(game_multiplier - 1.5 , 0.0 , 1.0)
-	level = clamp(game_multiplier * 10 , 5 , 40)
-	label_score.text = str(game_score)
-	label_score_multiplier.text = "[shake rate=%s level=%s connected=1]%sx[/shake]" %[shake_rate , level , game_multiplier]
+	shake_rate = clamp((multiplier - 1)/1.5 * 50, 0 , 50)
+	multiplier_fire_particles.amount_ratio = clamp(multiplier - 1.5 , 0.0 , 1.0)
+	level = clamp(multiplier/2.5 * 40, 5, 40)
+	label_score_multiplier.text = "[shake rate=%s level=%s connected=1]%sx[/shake]" %[shake_rate , level , multiplier]
+
+#func _update_text(game_score , game_multiplier):
+	#print("multiplicador:%s" %game_multiplier)
+	#print("shake rate:%s" %shake_rate)
+	#print("fire amount:%s" %multiplier_fire_particles.amount_ratio)
+	#print("level:%s" %level)
+	#var game_multiplier_percentage = game_multiplier/2.5
+	#_play_multi_animation()
+	#_play_score_animation()
+	#shake_rate = clamp((game_multiplier - 1)/1.5 * 50, 0 , 50)
+	#multiplier_fire_particles.amount_ratio = clamp(game_multiplier - 1.5 , 0.0 , 1.0)
+	#level = clamp(game_multiplier_percentage * 40, 5, 40)
+	#label_score.text = str(game_score)
+	#label_score_multiplier.text = "[shake rate=%s level=%s connected=1]%sx[/shake]" %[shake_rate , level , game_multiplier]
 
 func _play_multi_animation():
 	var multi_animations : Array = [
